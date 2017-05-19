2656aed6d327178ed72b7fcddaf3aa1fd8b7d7f3
Newton Joshua <newtonjoshua@rocketmail.com>
2017-05-19 17:28:43 +0530

    Lighthouse - to improve the quality of web page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCMqCyPwK9wUrbXysN2rcKmkbwwPZ2kMJs',
+    authDomain: 'warp-1494954802800.firebaseapp.com',
+    databaseURL: 'https://warp-1494954802800.firebaseio.com',
+    projectId: 'warp-1494954802800',
+    storageBucket: 'warp-1494954802800.appspot.com',
+    messagingSenderId: '204085206412'
+  }
 };
