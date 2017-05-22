a1935c34ddf469036dae6a749ccda90de3852d04
Newton Joshua <newtonjoshua@rocketmail.com>
2017-05-22 17:30:45 +0530

    Created a service for google maps api

--- src/app/google-maps-service/google-maps.service.ts
+++ src/app/google-maps-service/google-maps.service.ts
@@ -1,8 +1,162 @@
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GoogleMapsService {
 
+  private google = (<any>window).google;
+  private googleMap;
+  private googlePlacesService;
   constructor() { }
+
+  getLocation(): Observable<any> {
+    return Observable.create(observer => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          observer.next(pos);
+          observer.complete();
+        }, (error) => {
+          observer.error(error);
+        });
+      } else {
+        observer.error('Browser doesnt support Geolocation');
+      }
+    });
+
+  }
+
+  createMap(mapElement, position) {
+    this.googleMap = new this.google.maps.Map(mapElement, {
+      center: position,
+      zoom: 14,
+      mapTypeId: this.google.maps.MapTypeId.HYBRID,
+      disableDefaultUI: true
+    });
+
+    const trafficLayer = new this.google.maps.TrafficLayer();
+    trafficLayer.setMap(this.googleMap);
+    const transitLayer = new this.google.maps.TransitLayer();
+    transitLayer.setMap(this.googleMap);
+    this.googlePlacesService = new this.google.maps.places.PlacesService(this.googleMap);
+
+    const marker = new this.google.maps.Marker({
+      map: this.googleMap,
+      animation: this.google.maps.Animation.DROP,
+      position: position,
+      icon: {
+        path: this.google.maps.SymbolPath.CIRCLE,
+        scale: 8
+      }
+    });
+    marker.addListener('click', () => {
+      marker.setAnimation(this.google.maps.Animation.BOUNCE);
+      setTimeout(() => marker.setAnimation(null), 2000);
+    });
+  }
+
+  getLocationDetails(position): Observable<any> {
+
+    const latlng = new this.google.maps.LatLng(position.lat, position.lng);
+    const request = {
+      location: latlng,
+      radius: '50',
+      types: ['locality', 'neighborhood', 'political', 'street_number', 'administrative_area_level_2', 'administrative_area_level_3']
+    };
+
+    return Observable.create(observer => {
+      this.googlePlacesService.textSearch(request, (results, status) => {
+        let placePhoto;
+        if (status === 'OK') {
+          results.forEach(place => {
+            if (place.photos && !placePhoto && (place.types.includes('administrative_area_level_2') ||
+              place.types.includes('administrative_area_level_3'))) {
+              placePhoto = place.photos[0];
+              observer.next({
+                placeImageUrl: place.photos[0].getUrl({ 'maxHeight': 150 }),
+                placeAddress: place.formatted_address
+              });
+            }
+          });
+          if (!placePhoto) {
+            results.forEach(place => {
+              if (place.photos && !placePhoto && place.types.includes('administrative_area_level_1')) {
+                placePhoto = place.photos[0];
+                observer.next({
+                  placeImageUrl: place.photos[0].getUrl({ 'maxHeight': 150 }),
+                  placeAddress: place.formatted_address
+                });
+              }
+            });
+          }
+          observer.complete();
+        }
+      });
+    });
+  }
+
+  getRestaurantsList(position): Observable<any> {
+    return Observable.create(observer => {
+      this.googlePlacesService.nearbySearch({
+        location: position,
+        radius: 1000,
+        type: ['restaurant'], // Restricts the results to places matching restaurant
+      }, (restaurants, status) => {
+        if (status === this.google.maps.places.PlacesServiceStatus.OK) {
+          observer.next(restaurants);
+          observer.complete();
+        } else {
+          observer.error('Sorry. There are no Restaurants in your position.');
+        }
+      });
+    });
+  }
+
+  getRestaurantDetails(place): Observable<any> {
+    const request = {
+      placeId: place.place_id
+    };
+    return Observable.create(observer => {
+      this.googlePlacesService.getDetails(request, (placeDetails, status) => {
+        if (status === this.google.maps.places.PlacesServiceStatus.OK) {
+          const placeName = placeDetails.name.substring(0, 30);
+          observer.next({
+            location: placeDetails.geometry.location,
+            name: placeName,
+            open: placeDetails.opening_hours ? placeDetails.opening_hours.open_now : null,
+            photo: placeDetails.photos ? placeDetails.photos[0].getUrl({ 'maxHeight': 150 }) : null,
+            rating: placeDetails.rating,
+            url: placeDetails.website ? placeDetails.website : placeDetails.url,
+            address: placeDetails.vicinity,
+            phone: placeDetails.international_phone_number
+          });
+          this.createMarker(placeDetails.geometry.location, placeName);
+          observer.complete();
+        }
+      });
+    });
+  }
+
+  createMarker(location, placeName) {
+    const marker = new this.google.maps.Marker({
+      map: this.googleMap,
+      position: location,
+      animation: this.google.maps.Animation.DROP
+    });
+    const infoWindow = new this.google.maps.InfoWindow({
+      content: '<span style="color:black">' + placeName + '</span>'
+    });
+
+    this.google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+
+
+
+
 
 }
--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RestaurantModalComponent } from './../restaurant-modal/restaurant-modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GoogleMapsService } from './../google-maps-service/google-maps.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-restaurants',
@@ -11,162 +13,28 @@ import { GoogleMapsService } from './../google-maps-service/google-maps.service'
 })
 export class RestaurantsComponent implements OnInit {
 
-  private google;
-  private geocoder;
-  private map;
-  private infowindow;
-  private pos;
-  private service;
-  private placeImageUrl;
-  private placeAddress;
+  private placeImageUrl: Observable<string>;
+  private placeAddress: Observable<string>;
   private restaurantList = [];
 
   constructor(private modalService: NgbModal, private googleMapsService: GoogleMapsService) { }
 
   ngOnInit() {
-
-    this.google = (<any>window).google;
-    this.geocoder = new this.google.maps.Geocoder();
-    this.map = new this.google.maps.Map(document.getElementById('map'), {
-      center: { lat: 11, lng: 10 },
-      zoom: 14,
-      mapTypeId: 'satellite'
-    });
-    this.locate();
-  }
-
-  // Try HTML5 geolocation.
-  locate() {
-    this.restaurantList = [];
-    const that = this;
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        that.pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        this.getPlaceName(position);
-
-        that.map = new that.google.maps.Map(document.getElementById('map'), {
-          center: that.pos,
-          zoom: 14,
-          mapTypeId: that.google.maps.MapTypeId.HYBRID,
-          disableDefaultUI: true
-        });
-
-        const trafficLayer = new that.google.maps.TrafficLayer();
-        trafficLayer.setMap(that.map);
-        const transitLayer = new that.google.maps.TransitLayer();
-        transitLayer.setMap(that.map);
-
-        const marker = new that.google.maps.Marker({
-          map: that.map,
-          animation: that.google.maps.Animation.DROP,
-          position: that.pos,
-          icon: {
-            path: that.google.maps.SymbolPath.CIRCLE,
-            scale: 8
-          }
-        });
-        marker.addListener('click', () => {
-          marker.setAnimation(this.google.maps.Animation.BOUNCE);
-          setTimeout(() => marker.setAnimation(null), 2000);
-        });
-
-        that.infowindow = new that.google.maps.InfoWindow();
-        that.service = new that.google.maps.places.PlacesService(that.map);
-        that.service.nearbySearch({
-          location: that.pos,
-          radius: 1000,
-          type: ['restaurant'], // Restricts the results to places matching restaurant
-        }, (results, status) => {
-          if (status === that.google.maps.places.PlacesServiceStatus.OK) {
-            for (let i = 0; i < results.length; i++) {
-              this.getPlaceDetails(results[i]);
-            }
-          }
-        });
-
-      }, () => {
-        // The Geolocation service failed.
+    this.googleMapsService.getLocation().subscribe(position => {
+      this.googleMapsService.createMap(document.getElementById('map'), position);
+      this.googleMapsService.getLocationDetails(position).subscribe(locationDetails => {
+        this.placeImageUrl = Observable.of(locationDetails.placeImageUrl);
+        this.placeAddress = Observable.of(locationDetails.placeAddress);
       });
-    } else {
-      // Browser doesn't support Geolocation
-    }
-  }
-
-  getPlaceName(position) {
-    const latlng = new this.google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    const request = {
-      location: latlng,
-      radius: '50',
-      types: ['locality', 'neighborhood', 'political', 'street_number', 'administrative_area_level_2', 'administrative_area_level_3']
-    };
-
-    this.service = new this.google.maps.places.PlacesService(this.map);
-
-    this.service.textSearch(request, (results, status) => {
-      let placePhoto;
-      if (status === 'OK') {
-        results.forEach(place => {
-          if (place.photos && !placePhoto && (place.types.includes('administrative_area_level_2') ||
-            place.types.includes('administrative_area_level_3'))) {
-            placePhoto = place.photos[0];
-            this.placeImageUrl = placePhoto.getUrl({ 'maxHeight': 150 });
-            this.placeAddress = place.formatted_address;
-          }
-        });
-        if (!placePhoto) {
-          results.forEach(place => {
-            if (place.photos && !placePhoto && place.types.includes('administrative_area_level_1')) {
-              placePhoto = place.photos[0];
-              this.placeImageUrl = placePhoto.getUrl({ 'maxHeight': 150 });
-              this.placeAddress = place.formatted_address;
-            }
+      this.googleMapsService.getRestaurantsList(position).subscribe(restaurants => {
+        restaurants.forEach(restaurant => {
+          this.googleMapsService.getRestaurantDetails(restaurant).subscribe(restaurantDetails => {
+            this.restaurantList.push(restaurantDetails);
           });
-        }
-      }
-    });
-  }
-
-  createMarker(placeLoc, placeName) {
-    const that = this;
-    const marker = new this.google.maps.Marker({
-      map: this.map,
-      position: placeLoc,
-      animation: that.google.maps.Animation.DROP
-    });
-    const infoWindow = new this.google.maps.InfoWindow({
-      content: '<span style="color:black">' + placeName + '</span>'
-    });
-
-    this.google.maps.event.addListener(marker, 'click', () => {
-      infoWindow.open(this.map, marker);
-    });
-  }
-
-  getPlaceDetails(place) {
-    const request = {
-      placeId: place.place_id
-    };
-    return this.service.getDetails(request, (placeDetails, status) => {
-      if (status === this.google.maps.places.PlacesServiceStatus.OK) {
-        const placeName = placeDetails.name.substring(0, 50);
-        this.restaurantList.push({
-          location: placeDetails.geometry.location,
-          name: placeName,
-          open: placeDetails.opening_hours ? placeDetails.opening_hours.open_now : null,
-          photo: placeDetails.photos ? placeDetails.photos[0].getUrl({ 'maxHeight': 150 }) : null,
-          rating: placeDetails.rating,
-          url: placeDetails.website ? placeDetails.website : placeDetails.url,
-          address: placeDetails.vicinity,
-          phone: placeDetails.international_phone_number
         });
-        this.createMarker(placeDetails.geometry.location, placeName);
-      }
+      });
     });
   }
-
 
   openModal() {
     const modalRef = this.modalService.open(RestaurantModalComponent, { size: 'lg', backdrop: 'static' });
